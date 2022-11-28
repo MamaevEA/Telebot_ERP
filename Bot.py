ea4b4c0af8d26e718aebbea42f59e0374c597523
MamaevEA <mamaevea@gmail.com>
2022-11-28 17:52:36 +0500

    Add Pot

--- Bot.py
+++ Bot.py
@@ -4,6 +4,7 @@
 
 import telebot
 import json
+import random
 
 API_TOKEN='5925350659:AAHpjfR8i-gah2l02XABQTsMSpDfBt2sSSU'
 
@@ -11,39 +12,80 @@ bot = telebot.TeleBot(API_TOKEN)
 
 calc = False
 data = {}
+def uniqueid():
+    seed = random.getrandbits(32)
+    while True:
+       yield seed
+       seed += 1
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     global data
-    with open('ERP/data.json', encoding='utf-8') as json_file:
+    with open('data.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Привет', 'Пока')
-    bot.send_message(message.chat.id, 'Готов к работе!', reply_markup=keyboard) 
-
-@bot.message_handler(commands=['menu'])
-def show_message(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Привет', 'Пока')
-    bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
-
-@bot.message_handler(commands=['calc'])
-def calc_message(message):
-    global calc
-    #eq = message.text.split()[1:]
-    #print(eq[0])
-    #print(eval(eq[0]))
-    calc=True
-    bot.send_message(message.chat.id,'А теперь введите выражение для расчета')    
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('/Команды', '/Справочник')
+        bot.send_message(message.chat.id, 'Привет! Что будем делать с телефонным справочником?', reply_markup=keyboard)
+    
+@bot.message_handler(commands=['Команды'])
+def comands_message(message):
+    bot.send_message(message.chat.id,'Чтобы добавить новый контакт, введите команду: \n \
+        /Добавить, Ф.И.О. через пробел и номер телефона \n \
+            (/Добавить Иванов Иван Иванович 89601234567)')
+    bot.send_message(message.chat.id,'Чтобы удалить контакт, введите команду: \n \
+        /Удалить и через ID контакта  \n \
+            (/Удалить 3474373417)')
+    bot.send_message(message.chat.id,'Чтобы найти контакт, введите команду: \n \
+        /Найти и ID контакта через пробел \n \
+            (/Найти 3474373417)')
+        
+@bot.message_handler(commands=['Справочник'])
+def comands_message(message):
+    global data
+    with open('data.json', 'rb') as json_file:
+        data = json_file.read()
+    bot.send_message(message.chat.id, data)
 
 @bot.message_handler(content_types='text')
 def check_message(message):
     global calc
-    if 'привет' in message.text:
-        bot.send_message(message.chat.id,'И тебе привет коли не шутишь!')  
-    if calc:
-         calc=False
-         bot.send_message(message.chat.id,'результат равен '+str(eval(message.text)))
-
+    if '/Добавить' in message.text:
+        bot.send_message(message.chat.id,'Добавили!')
+        msg = message.text
+        msg = msg.split(' ')
+        name = [msg[1], msg[2], msg[3]]
+        name = ' '.join(name)
+        unique_sequence = uniqueid()
+        ID = next(unique_sequence)
+        with open('data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file)
+        data[ID] = {
+            'Name': name,
+            'Phone': msg[4]
+        }
+        with open('data.json', 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, indent=2, ensure_ascii=False)
+    elif '/Удалить' in message.text:
+        bot.send_message(message.chat.id,'Удалили!')
+        msg = message.text
+        msg = msg.split(' ')
+        with open('data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file) 
+        data.pop(msg[1], None)
+        with open('data.json', 'w', encoding='utf-8') as outfile:
+            json.dump(data, outfile, indent=2, ensure_ascii=False)
+    elif '/Найти' in message.text:
+        bot.send_message(message.chat.id,'Результат поиска:')
+        msg = message.text
+        msg = msg.split(' ')
+        msg = msg[1]
+        print(msg)
+        with open('data.json', encoding='utf-8') as json_file:
+            data = {}
+            data = json.load(json_file)
+            search = data[msg]
+        bot.send_message(message.chat.id, str(search))
 
 bot.polling()
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Telebot_ERP
+
+## Урок 10. Возможна ли жизнь без PIP? Продолжение
+Прикрутить бота к задачам с предыдущего семинара:
+
+или Информационная система
+
+или Телефонный справочник с возможностями работы с информацией, и экспорта информации.
+
+# Результат:
+ТГ бот умеет урезанный функционал по работе с телефонным справочником.
+
+## Функции:
+1. Просмотреть весь справочник (кнопка)
+2. Вывести список доступных команд (кнопка)
+3. Добавить контакт (ФИО и телефон)
+4. Удалить контакт
+5. Найти контакт по ID
+
+![Start](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Start.png)
+Первичная страница бота"
+---
+
+![Start_messege](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Start_messege.png)
+Старт работы бота"
+---
+![All](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/All.png)
+Вывод телефонного справочника"
+---
+![Comands](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Comands.png)
+Вывод списка доступных команд"
+---
+![Add_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Add_people.png)
+Добавление контакта"
+---
+![Search_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Add_people.png)
+Поиск контакта"
+---
+![Dell_people](https://github.com/MamaevEA/Telebot_ERP/blob/main/Picturs/Dell_people.png)
+Удаление контакта"
+---
